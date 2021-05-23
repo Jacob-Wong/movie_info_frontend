0cfee2cd74c614d9db58dfc2cdc195fc99d3261e
Jacob-Wong <jacobwfk@gmail.com>
2021-05-24 04:57:37 +0800

    Create MovieCard.js

---
+++ src/components/MovieCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const MovieCard = (props) => {
+  const movie = props.movie;
+
+  return (
+    <div className="card-container">
+      <img
+        // src="https://commapress.co.uk/books/the-book-of-cairo/cairo-provisional-v3/image%2Fspan3"
+        src="https://drive.google.com/uc?id=1_KtfvGx5yEKq2BuIILnqKa2phxat77fZ"
+        alt=""
+      />
+      <div className="desc">
+        <h2>
+          <Link to={`/show-movie/${movie._id}`}>{movie.title}</Link>
+        </h2>
+        <h3>{movie.production_companies}</h3>
+        <p>{movie.original_language}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
